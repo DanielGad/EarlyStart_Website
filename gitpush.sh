d7b2277054bb1e7abfcedcee8810fa641fd701a3
DanielGad <adeyemidanielgad@gmail.com>
2024-12-13 13:05:45 +0100

    Language update

--- gitpush.sh
+++ gitpush.sh
@@ -2,23 +2,17 @@
 up
 echo "Current Directory: $(pwd)"
 
-
 if [ ! -d ".git" ]; then
   echo "Not a git repository. Please run this script inside a git repository."
   exit 1
 fi
 
-
 git add --all
-
 
 read -p "Enter commit message: " commit_message
 
-
 git commit -m "$commit_message"
 
-
 git push
-
 
 echo "Changes have been committed and pushed!"
