6ef21cce3b8c7880f01414e19cb640aff39e8c3b
DanielGad <adeyemidanielgad@gmail.com>
2025-02-26 11:16:32 +0100

    Added Payment Checks

--- frontend/src/components/Homepage/Home.tsx
+++ frontend/src/components/Homepage/Home.tsx
@@ -3,7 +3,13 @@ import './Home.css';
 import Helping_Mother from '../../assets/images/hp-mother-helping.png';
 import CEO from '../../assets/images/CEO.jpeg';
 import Smiley_Male from '../../assets/images/PlatformsOnline.jpg';
-import Smiley_Female from '../../assets/images/OnlineClass.jpg'
+import Smiley_Female from '../../assets/images/OnlineClass.jpg';
+import Tutor1 from '../../assets/images/tutor1.png';
+import Tutor2 from '../../assets/images/tutor2.jpg';
+import Tutor3 from '../../assets/images/tutor3.jpg';
+import Tutor4 from '../../assets/images/tutor4.jpg';
+import Tutor5 from '../../assets/images/tutor5.png';
+import Tutor6 from '../../assets/images/tutor6.jpg';
 import ArrowDropRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import NumberWork from '../../assets/images/hp-numberwork.png';
 import HomeWork from '../../assets/images/hp-homework.png';
@@ -15,6 +21,9 @@ import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { Context } from '../../Context/Context';
 import Footer from '../Footer/Footer';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { useMediaQuery } from 'react-responsive';
 
 const Home = () => {
   useEffect(() => {
@@ -25,148 +34,179 @@ const Home = () => {
   if (!context) {
     throw new Error("Must be used within Context provider");
   }
-  const { setMenu } = context
+  const { setMenu } = context;
+
+  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 768px)' });
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
 
   return (
     <div>
-    <div className="home-container">
-      <div className="home-home-1">
-        <div className="home-1-left-col">
-          <div className='learn'>
-            Learn Anywhere, Anytime with EarlyStart E-Tutors
+      <div className="home-container">
+        <div className="home-home-1">
+          <div className="home-1-left-col">
+            <div className='learn'>
+              Learn Anywhere, Anytime with EarlyStart E-Tutors
+            </div>
+            <div className='enhance'>
+              Enhance your child&apos;s education with personalized online tutoring from EarlyStart E-Tutors. Connect with experienced teachers for your child to ensure high quality teaching-learning process from the comfort of your home.
+            </div>
+            <Link to={"/get-started"}>
+              <button className="get-tutor-button">Get Started Now! <ArrowDropRightIcon className='arrow-right' /></button>
+            </Link>
           </div>
-          <div className='enhance'>
-          Enhance your child&apos;s education with personalized online tutoring from EarlyStart E-Tutors. Connect with experienced teachers for your child to ensure high quality teaching-learning process from the comfort of your home.
-          </div>
-          <Link to={"/get-started"}>
-          <button className="get-tutor-button">Get Started Now! <ArrowDropRightIcon className='arrow-right' /></button>
-          </Link>
-        </div>
 
           <div className="home-1-image-cont">
             <img src={Helping_Mother} alt="Helping Mother" />
           </div>
+        </div>
 
-      </div>
-
-      {/*Our Services */}
-      <div className="home-services">
-        Our Services
-      </div>
-      <div className="home-2">
-          <div className="home-2-con" onClick={() => {setMenu("service")}}>
+        {/* Our Services */}
+        <div className="home-services">
+          Our Services
+        </div>
+        <div className="home-2">
+          <div className="home-2-con" onClick={() => { setMenu("service") }}>
             <Link to={"/mathematics"}>
-            <div className="home-2-icon">
-              <img src={NumberWork} alt="text icon" width={"50px"}/>
-            </div>
-            <div className="home-2-texts">
-              Number Work/ Quantitative Reasoning/ Mathematics
-            </div>
+              <div className="home-2-icon">
+                <img src={NumberWork} alt="text icon" width={"50px"} />
+              </div>
+              <div className="home-2-texts">
+                Number Work/ Quantitative Reasoning/ Mathematics
+              </div>
             </Link>
           </div>
 
-          <div className="home-2-con" onClick={() => {setMenu("service")}}>
+          <div className="home-2-con" onClick={() => { setMenu("service") }}>
             <Link to={"/letterwork"}>
-            <div className="home-2-icon">
-              <img src={LetterWork} alt="text icon" width={"50px"}/>
-            </div>
-            <div className="home-2-texts">
-              Letter Work/ Word Formation/ Alphabet Sounds/ Phonetics/ Verbal Reasoning
-            </div>
+              <div className="home-2-icon">
+                <img src={LetterWork} alt="text icon" width={"50px"} />
+              </div>
+              <div className="home-2-texts">
+                Letter Work/ Word Formation/ Alphabet Sounds/ Phonetics/ Verbal Reasoning
+              </div>
             </Link>
           </div>
-          
-          <div className="home-2-con" onClick={() => {setMenu("service")}}>
+
+          <div className="home-2-con" onClick={() => { setMenu("service") }}>
             <Link to={"/language"}>
-            <div className="home-2-icon">
-              <img src={Language} alt="text icon" width={"50px"}/>
-            </div>
-            <div className="home-2-texts">
-              Languages/ French/ English/ Yoruba/ Igbo/ Hausa
-            </div>
+              <div className="home-2-icon">
+                <img src={Language} alt="text icon" width={"50px"} />
+              </div>
+              <div className="home-2-texts">
+                Languages/ French/ English/ Yoruba/ Igbo/ Hausa
+              </div>
             </Link>
           </div>
 
-          <div className="home-2-con" onClick={() => {setMenu("service")}}>
+          <div className="home-2-con" onClick={() => { setMenu("service") }}>
             <Link to={"/homework"}>
-            <div className="home-2-icon">
-              <img src={HomeWork} alt="text icon" width={"50px"}/>
-            </div>
-            <div className="home-2-texts">
-              Home Work/ School Assignment
-            </div>
+              <div className="home-2-icon">
+                <img src={HomeWork} alt="text icon" width={"50px"} />
+              </div>
+              <div className="home-2-texts">
+                Home Work/ School Assignment
+              </div>
             </Link>
           </div>
 
-          <div className="home-2-con" onClick={() => {setMenu("service")}}>
+          <div className="home-2-con" onClick={() => { setMenu("service") }}>
             <Link to={"/general"}>
-            <div className="home-2-icon">
-              
-              <img src={General} alt="text icon" width={"50px"}/>
-            </div>
-            <div className="home-2-texts">
-              General Knowledge/ Vocational Aptitude
-            </div>
+              <div className="home-2-icon">
+                <img src={General} alt="text icon" width={"50px"} />
+              </div>
+              <div className="home-2-texts">
+                General Knowledge/ Vocational Aptitude
+              </div>
             </Link>
           </div>
 
-          <div className="home-2-con" onClick={() => {setMenu("service")}}>
+          <div className="home-2-con" onClick={() => { setMenu("service") }}>
             <Link to={"/international"}>
-            <div className="home-2-icon">
-              <img src={International} alt="text icon" width={"50px"}/>
-            </div>
-            <div className="home-2-texts">
-              International School Enterance Preparation
-            </div>
+              <div className="home-2-icon">
+                <img src={International} alt="text icon" width={"50px"} />
+              </div>
+              <div className="home-2-texts">
+                International School Entrance Preparation
+              </div>
             </Link>
           </div>
-         
         </div>
 
         {/* Who we are */}
         <div className="home-3">
           <div className="home-3-image-con">
-            <img src={CEO} alt="CEO" style={{height: "auto"}} />
+            <img src={CEO} alt="CEO" style={{ height: "auto" }} />
             <div className='ceo'>Christianah Yemisi Awe</div>
-            <div className='ceo' style={{marginTop: "-20px"}}>Proprietor</div>
+            <div className='ceo' style={{ marginTop: "-20px" }}>Proprietor</div>
           </div>
           <div className="home-3-text">
             <div className="who-we-are">
               Who We Are
             </div>
-            <div className="home-3-text-cont">          
+            <div className="home-3-text-cont">
               EarlyStart E-Tutors connects elementary school pupils with expert tutors through personalized, interactive online learning. We specialize in matching learners with qualified educators for individual academic support and specialized programs. Our goal is to enhance each child educational ability and learning skills from the comfort of home.
             </div>
             <Link to={"/get-started"}>
-          <button className="get-tutor-button" style={{padding: "20px", marginTop: "20px"}}>Get Started Now! <ArrowDropRightIcon className='arrow-right' /></button>
-          </Link>
+              <button className="get-tutor-button" style={{ padding: "20px", marginTop: "20px" }}>Get Started Now! <ArrowDropRightIcon className='arrow-right' /></button>
+            </Link>
           </div>
+        </div>
+
+        <div className="gallery">
+          <Carousel
+            showThumbs={false}
+            autoPlay
+            infiniteLoop
+            showStatus={false}
+            showIndicators={false}
+            centerMode
+            centerSlidePercentage={isDesktopOrLaptop ? 33.33 : 90}
+          >
+            <div>
+              <img src={Tutor1} alt="Image 1" />
+            </div>
+            <div>
+              <img src={Tutor2} alt="Image 2" />
+            </div>
+            <div>
+              <img src={Tutor3} alt="Image 3" />
+            </div>
+            <div>
+              <img src={Tutor4} alt="Image 4" />
+            </div>
+            <div>
+              <img src={Tutor5} alt="Image 5" />
+            </div>
+            <div>
+              <img src={Tutor6} alt="Image 6" />
+            </div>
+          </Carousel>
         </div>
 
         {/* Join Us */}
         <div className="home-4">
           <div className="home-4-image-con-1">
-          <img src={Smiley_Male} alt="Smiley Male" />
+            <img src={Smiley_Male} alt="Smiley Male" />
           </div>
-          <div className="join-us-text-con" style={{marginLeft: "2 0px"}}>
+          <div className="join-us-text-con" style={{ marginLeft: "20px" }}>
             <div className="join-us-text">
               Join EarlyStart E-Tutors and earn money by teaching what you love. Share your expertise and help young learners thrive.
             </div>
-            <div style={{marginTop: "20px", fontSize: "1.4rem"}}>
-              To Join? 
+            <div style={{ marginTop: "20px", fontSize: "1.4rem" }}>
+              To Join?
             </div>
-            <div style={{marginTop: "20px", fontSize: "1.2rem"}}>
-              Kindly send your Curriculum Vitae (CV) and a passport photograph attached to <a href="mailto:earlystartetutors@gmail.com" style={{ color: "white", textDecoration: "underline"}}>this email account - earlystartetutors@gmail.com</a>
+            <div style={{ marginTop: "20px", fontSize: "1.2rem" }}>
+              Kindly send your Curriculum Vitae (CV) and a passport photograph attached to <a href="mailto:earlystartetutors@gmail.com" style={{ color: "white", textDecoration: "underline" }}>this email account - earlystartetutors@gmail.com</a>
             </div>
           </div>
           <div className="home-4-image-con-2">
             <img src={Smiley_Female} alt="Smiley Female" />
           </div>
         </div>
+      </div>
+      <Footer />
     </div>
-    <Footer />
-    </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
