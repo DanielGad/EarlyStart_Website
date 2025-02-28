b76812b01d5099d7304f73143a8b5fe9a14d5d0c
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2025-02-28 11:06:46 +0100

    Update Action.tsx

--- frontend/src/components/Action/Action.tsx
+++ frontend/src/components/Action/Action.tsx
@@ -14,9 +14,9 @@ const Action = () => {
       <div className="action-divide">
         <div className="action-left">
           <h3 className='steps'>1. Get Started with a Click</h3>
-          <p className='steps-sub'>Begin your journey by visiting our website and signing up. It's quick and easy</p>
+          <p className='steps-sub'>Begin your journey by visiting our website and signing up. It's quick and easy.</p>
           <h3 className='steps'>2. Specify Your Child's Needs</h3>
-          <p className='steps-sub'>Input the area where your child requires tutoring, whether it's Number Work, Quantitative Reasoning, Mathematics, Letter Work, Word Formation, Phonetics, Languages and so on.</p>
+          <p className='steps-sub'>Input the area where your child requires tutoring, whether it's Number Work, Quantitative Reasoning, Mathematics, Letter Work, Word Formation, Phonetics, Languages etc.</p>
           <h3 className='steps'>3. Choose Your Schedule</h3>
           <p className='steps-sub'>Select your preferred schedule time, either weekdays or weekends, to fit your family's routine.</p>
           <h3 className='steps'>4. Get Paired with a Tutor</h3>
